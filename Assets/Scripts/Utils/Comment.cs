ef33f67da41250092d5abde8b96985117ffa7999
Nick Shalimov <nnshalimov@gmail.com>
2018-02-23 16:39:08 +0300

    demo updates

---
+++ Assets/Scripts/Utils/Comment.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    [ExecuteInEditMode]
+    public class Comment: MonoBehaviour
+    {
+        [TextArea(3, 5)]
+        [SerializeField] private string _text;
+
+        private void Awake()
+        {
+            tag = "EditorOnly";
+        }
+    }
+}
